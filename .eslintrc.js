9f65e13e885629f95bc8c9e66329f895bddb9b7c
cristian <cristianmr.28@gmail.com>
2022-05-25 17:46:16 +0000

    Update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,13 +18,17 @@ module.exports = {
 	plugins: ['@typescript-eslint', 'prettier'],
 	rules: {
 		'comma-dangle': ['error', 'always-multiline'],
-		indent: ['error', 'tab'],
+		indent: ['error', 'tab', { SwitchCase: 1 }],
 		'linebreak-style': ['error', 'unix'],
-		'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
 		'quote-props': ['error', 'as-needed'],
-		quotes: ['error', 'single'],
+		// quotes: ['error', 'single', 'as-needed'],
 		semi: ['error', 'always'],
 		// 'sort-keys': ['error', 'asc', { caseSensitive: true, minKeys: 2, natural: false }],
 		'spaced-comment': ['warn', 'always'],
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{ argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+		],
 	},
 };
